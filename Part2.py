aefe713ee21c3d88d0efcf14dddbc832ce309b3f
austin <aclyde11@gmail.com>
2019-02-08 11:50:22 -0600

    Part1 Done

--- Part2.py
+++ Part2.py
@@ -47,7 +47,7 @@ def part1_run_model_and_report_class_stats(X, Y, clf, CV=5):
         score_dict['acc'] = clf.score(X[test_index, :], Y[test_index])
         score_dict['r2'] = metrics.r2_score(Y[test_index], y_pred)
         score_dict['confusion'] = metrics.confusion_matrix(Y[test_index], y_pred)
-        score_dict['f1'] = metrics.f1_score(Y[test_index], y_pred)
+        score_dict['f1'] = metrics.f1_score(Y[test_index], y_pred, average='macro')
 
         scores = pd.concat([scores, pd.DataFrame([score_dict], columns=score_dict.keys())], sort=True)
     return scores.reset_index().drop("index", axis=1)
@@ -79,21 +79,17 @@ def part_b(X_all, X_coding, Y):
 
     # This list of classifers is stolen directly from the example on Sklearn "Classifcation comparison"
     # https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
-    names = ["Logistic Regression", "Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
-             "Decision Tree", "Random Forest", "AdaBoost",
-             "Naive Bayes", "QDA"]
+    names = ["Nearest Neighbors", "Linear SVM",
+             "Decision Tree", "Random Forest", "AdaBoost"]
 
     classifiers = [
-        LogisticRegression(),
+        # LogisticRegression(multi_class='ovr'),
         KNeighborsClassifier(4, n_jobs=-1),
         SVC(kernel="linear"),
-        SVC(kernel="rbf", gamma='auto'),
-        GaussianProcessClassifier(1.0 * RBF(1.0), n_jobs=-1),
         DecisionTreeClassifier(),
         RandomForestClassifier(n_jobs=-1),
         AdaBoostClassifier(),
-        GaussianNB(),
-        QuadraticDiscriminantAnalysis()]
+    ]
 
     scores = []
 
@@ -103,7 +99,8 @@ def part_b(X_all, X_coding, Y):
     for name, clf in tqdm(zip(names, classifiers)):
         score = part1_run_model_and_report_class_stats(X_coding_scaled, Y, clf)
         scores.append(score.sort_values("acc").iloc[0])
-    print(tabulate.tabulate(pd.DataFrame(scores, index=names).sort_values("acc"), headers='keys', tablefmt='psql'))
+    df = pd.DataFrame(scores, index=names)
+    print(tabulate.tabulate(df.sort_values("acc"), headers='keys', tablefmt='psql'))
 
 
 def part_c(X_all, X_coding, Y):
@@ -134,11 +131,12 @@ def part_d_run_model(X_coding, Y, Y_holdout, X_holdout):
     from keras.layers import Input, Dense, Dropout
     from keras.models import Model
 
+    y_train = keras.utils.to_categorical(Y, len(np.unique(Y)))
     input = Input((X_coding.shape[1],))
     x = Dense(100, activation='relu', kernel_initializer='uniform')(input)
     x = Dense(50, activation='relu', kernel_initializer='uniform')(x)
     x = Dense(50, activation='relu', kernel_initializer='uniform')(x)
-    x = Dense(2, activation='softmax', kernel_initializer='uniform')(x)
+    x = Dense(len(np.unique(Y)), activation='softmax', kernel_initializer='uniform')(x)
 
     model = Model(inputs=input, outputs=x)
     model.compile(optimizer=keras.optimizers.SGD(lr=0.0005, momentum=0.9, nesterov=True),
@@ -159,7 +157,7 @@ def part_d_run_model(X_coding, Y, Y_holdout, X_holdout):
     score_dict['acc'] = metrics.accuracy_score(Y_test_int, Y_pred_int)
     score_dict['r2'] = metrics.r2_score(Y_test_int, Y_pred_int)
     score_dict['confusion'] = metrics.confusion_matrix(Y_test_int, Y_pred_int)
-    score_dict['f1'] = metrics.f1_score(Y_test_int, Y_pred_int)
+    score_dict['f1'] = metrics.f1_score(Y_test_int, Y_pred_int, average='macro')
     return score_dict
 
 
@@ -193,21 +191,13 @@ def part_d(X_all, X_coding, Y):
     plt.savefig('part1_d_learning_curve.png')
     plt.show()
 
-def part_1_ec(X_all, X_coding, Y):
-    from tpot import TPOTClassifier
-    from sklearn.model_selection import train_test_split
-    pipeline_optimizer = TPOTClassifier(generations=5, population_size=20, cv=5,n_jobs=4,
-                                        random_state=42, verbosity=2, max_time_mins=60)
 
-    X_train, X_test, y_train, y_test = train_test_split(X_coding, Y, stratify=Y, random_state=42)
-    pipeline_optimizer.fit(X_train, y_train)
-    print(pipeline_optimizer.score(X_test, y_test))
 
 
 if __name__ == "__main__":
     args = arg_setup()
-    X_coding, Y_coding = load_data(args.data + "nt.coding.csv")
-    X_all, Y_all = load_data(args.data + "nt.all.csv")
+    X_coding, Y_coding = load_data(args.data + "type.coding.csv")
+    X_all, Y_all = load_data(args.data + "type.all.csv")
 
     Y = None
     if np.all(Y_all != Y_coding):
@@ -218,7 +208,6 @@ if __name__ == "__main__":
         del Y_all
         del Y_coding
 
-    # for part in 'abcd':
-    #     print_heading("PART %s" % part)
-    #     locals()["part_%s" % part](X_all, X_coding, Y)
-    part_1_ec(X_all, X_coding, Y)
+    for part in 'bcd':
+        print_heading("PART %s" % part)
+        locals()["part_%s" % part](X_all, X_coding, Y)
